1806c73ca2d8eae79353a482e6ea2fdc13ea6e3c
Kevin Chamorro <kevin.j03@hotmail.com>
2024-07-17 00:40:32 -0500

    Configuraciones iniciales products

---
+++ src/products/products.service.ts
@@ -0,0 +1,74 @@
+import { Injectable, InternalServerErrorException, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+
+  private readonly logger = new Logger('ProductService');
+
+  onModuleInit() {
+    this.$connect();
+    this.logger.log("Database connected.");
+  }
+
+  create(createProductDto: CreateProductDto) {
+    return this.product.create({
+      data: createProductDto
+    });
+  }
+
+  async findAll(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
+    const totalRecords = await this.product.count({ where: { status: 'E' } });
+    return {
+      data: await this.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: { status: 'E' },
+      }),
+      meta: {
+        total: totalRecords,
+        page,
+        lastPage: Math.ceil(totalRecords / limit),
+      }
+    };
+  }
+
+  async findOne(id: number) {
+    const product = await this.product.findFirst({
+      where: { id, status: 'E' }
+    });
+    if (!product) {
+      throw new NotFoundException(`Product with id #${id} not found.`);
+    }
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    try {
+      const { id: __, ...data } = updateProductDto;
+      return await this.product.update({
+        where: { id, status: 'E' },
+        data: data
+      })
+    } catch (error) {
+      throw new InternalServerErrorException(`Error updating product`);
+    }
+  }
+
+  async remove(id: number) {
+    try {
+      return await this.product.update({
+        where: { id, status: 'E' },
+        data: {
+          status: 'D'
+        }
+      })
+    } catch (error) {
+      throw new InternalServerErrorException(`Error deleting product`);
+    }
+  }
+}
